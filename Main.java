ae1a58feeb9e044451260179981cab5b2e3e7b75
igmezencev <swd-w11@yandex.ru>
2016-10-08 12:04:09 +0500

    123123

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * //Прописываем, какой нужно использовать метод при нажатии на кнопку
  */
 package lenghttext;
 
