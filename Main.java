1942bef46c1a1d4f16b079ec7ed28aa717a5c506
esvlad <svladpro@yandex.ru>
2016-10-08 11:39:14 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lenghttext;
+
+//Имортируем компонент JOptionPane пакета javax.swing
+import javax.swing.JOptionPane;
+
+/**
+ * @author Старцев Владислав, Рустем Еникеев, Игорь Мезенцев, Рустам Хайруллин
+ */
+
+//Инициализируем главный класс программы
+public class Main extends javax.swing.JFrame {
+
+}
